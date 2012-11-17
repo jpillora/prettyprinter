8e39ed16c752d483e7e511921e03c13843b3e239
Jaime Pillora <jpillora@gmail.com>
2012-11-18 01:58:19 +1100

    fixed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 //App entry point
-define(['jquery','lib/prettify'], function() {
+define(['lib/prettify','jquery'], function(prettify) {
 
   function htmlEncode(value){
     return $('<div/>').text(value).html();
   }
 
-  $("textarea").keyup(function() {
-    $("pre").html(htmlEncode($(this).val()));
-    window.prettyPrint();
-  });
+  var update = function() {
+
+    var code = htmlEncode($(this).val());
+
+    code = $(prettify.prettyPrintOne(code));
+    //loop through spans and swap out space chars for html
+    code.each(function() {
+      $(this).html( 
+        $(this).html().replace(/\n/g,'<br/>').replace(/\ /g,'&nbsp;')
+      );
+    });
+
+    $("pre").html(code);
+  }
+  
+  $("textarea").keyup(update);
 
 });
